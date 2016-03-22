a1c98972db2cb6723ad3318f9b4cb28232363eca
Daniel Duarte <dduarte@goplaceit.com>
2016-03-22 12:38:03 -0300

    logic for BoardsController

--- js/controllers/BoardsController.js
+++ js/controllers/BoardsController.js
@@ -2,6 +2,46 @@
 ; // The magic
 
 bbApp.controller('BoardsController', function(){
-  
+  var self = this;
+
+  self.data = [
+    {
+      id: 1,
+      name: 'Por hacer',
+      ideas: [
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() }
+      ]
+    },
+    {
+      id: 2,
+      name: 'En proceso',
+      ideas: [
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Hechas',
+      ideas: [
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() },
+        { text: 'Lorem ipsum dolor sit amet', createdAt: Date.now() }
+      ]
+    },
+  ];
+
 });
 
