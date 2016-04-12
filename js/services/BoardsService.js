0e7f3cfb13b30855eff59cd253e84e35c3d52033
Dani Duarte <elduarte@gmail.com>
2016-04-12 16:53:09 -0300

    fixing previous commit

--- js/services/BoardsService.js
+++ js/services/BoardsService.js
@@ -5,17 +5,14 @@ bbApp.factory('BoardsService', function(){
 
   var data = [
     {
-      // id: 1,
       name: 'Por hacer',
       ideas: []
     },
     {
-      // id: 2,
       name: 'En proceso',
       ideas: []
     },
     {
-      // id: 3,
       name: 'Hechas',
       ideas: []
     },
@@ -26,7 +23,7 @@ bbApp.factory('BoardsService', function(){
 
     addIdeaToBoard: function(idea, board){
       var boardIndex = data.indexOf(board);
-      data[boardIndex].ideas.push(idea);
+      data[boardIndex].ideas.push(idea);git a
     },
     
     removeIdeaFromBoard: function(idea, board){
